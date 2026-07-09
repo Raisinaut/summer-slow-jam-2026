abb05d282635698aaa283307a597f22cc5e62a44
Zach Delgros <truegamer95@gmail.com>
2026-07-08 21:33:29 -0700

    More card animations

--- automatic_card_swap_idea/godot/gameplay/card/card.gd
+++ automatic_card_swap_idea/godot/gameplay/card/card.gd
@@ -8,10 +8,12 @@ var face_up : bool = false :
 		back.visible = not face_up
 		
 var scale_tween : Tween = null
-var idle_tween : Tween = null
+var mouseover_tween : Tween = null
 var size : Vector2 : set = set_size 
 
+@onready var visuals: Control = %Visuals
 @onready var panel: PanelContainer = %Panel
+@onready var shadow: Panel = %Shadow
 @onready var front: MarginContainer = %Front
 @onready var back: MarginContainer = %Back
 @onready var press_detection: Button = %PressDetection
@@ -19,15 +21,17 @@ var size : Vector2 : set = set_size
 func _ready() -> void:
 	face_up = face_up
 	press_detection.pressed.connect(flip)
+	press_detection.mouse_entered.connect(_on_press_detection_mouse_entered)
+	press_detection.mouse_exited.connect(_on_press_detection_mouse_exited)
 
 func flip() -> void:
 	if animating_flip():
 		return
 	scale_tween = create_tween().set_trans(Tween.TRANS_SINE)
 	scale_tween.set_ease(Tween.EASE_IN)
-	scale_tween.tween_property(self, "scale:x", 0, 0.2)
+	scale_tween.tween_property(visuals, "scale:x", 0, 0.2)
 	scale_tween.set_ease(Tween.EASE_OUT)
-	scale_tween.tween_property(self, "scale:x", 1, 0.2)
+	scale_tween.tween_property(visuals, "scale:x", 1, 0.2)
 	await scale_tween.step_finished
 	face_up = not face_up
 
@@ -37,6 +41,22 @@ func animating_flip() -> bool:
 func set_size(val) -> void:
 	panel.size = val
 	panel.position = -val * 0.5
+	shadow.size = panel.size
+	shadow.position = panel.position
+	press_detection.size = panel.size
+	press_detection.position = panel.position
 
 func get_size() -> Vector2:
 	return panel.size
+
+func _on_press_detection_mouse_entered() -> void:
+	if mouseover_tween: mouseover_tween.kill()
+	mouseover_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	mouseover_tween.tween_property(panel, "offset_transform_position:y", -40, 0.1)
+	mouseover_tween.parallel().tween_property(panel, "offset_transform_scale", Vector2.ONE * 1.2, 0.1)
+
+func _on_press_detection_mouse_exited() -> void:
+	if mouseover_tween: mouseover_tween.kill()
+	mouseover_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	mouseover_tween.tween_property(panel, "offset_transform_position:y", 0, 0.1)
+	mouseover_tween.parallel().tween_property(panel, "offset_transform_scale", Vector2.ONE, 0.1)
